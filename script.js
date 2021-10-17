50dfc04aa281fceea90f38161b05cd02f1ce2c60
Annika Flyxe <anni842b@stud.kea.dk>
2021-10-17 20:37:52 +0200

    change

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function changeBg() {
     "url(img/img2.jpg)",
     "url(img/img3.jpg)",
     "url(img/img4.jpg)",
+    "url(img/img5.jpg)",
+    "url(img/img6.jpg)",
+    "url(img/img7.jpg)",
+    "url(img/img8.jpg)",
+    "url(img/img9.jpg)",
+    "url(img/img10.jpg)",
   ];
 
   const body = document.querySelector("body");
@@ -10,4 +16,4 @@ function changeBg() {
   body.style.backgroundImage = bg;
 }
 
-setInterval(changeBg, 3000);
+setInterval(changeBg, 4000);
